4247d11f5f5341497c25dfa283a45bcf2b6cb73f
patrictobola <tobola92@gmail.com>
2023-07-03 15:28:00 +0200

    imported pokemons in store

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <script>
+import axios from 'axios'
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons'
+import { store } from './data/store'
+import AppMain from './components/AppMain.vue';
+
+export default {
+  components: {
+    AppMain,
+  },
+
+  created() {
+    axios.get(endpoint).then(res => {
+      store.docs = res.data.docs;
+    })
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <AppMain />
+</template>
 
 <style></style>
